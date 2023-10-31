d6663ab22742d71df0542f1c248528f4f9759b29
decca1011 <deepakpatil101197@gmail.com>
2023-10-31 21:29:32 +0530

    writing typescript Express.js

---
+++ app.ts
@@ -0,0 +1,13 @@
+// const express = require('express');
+
+import express from 'express'
+
+import todosRoutes from './routes/todos';
+
+
+const app = express();
+
+app.use(todosRoutes);
+
+
+app.listen({ port: 3000 });
---
+++ routes/todos.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+const todos = [];
+
+router.get('/', (req,res,next) => {
+   res.status(200).json({todos: todos});
+});
+
+export default router;
+
+
